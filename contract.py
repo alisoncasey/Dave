ae0d8ec5848d8e70075cd597b91b60060499c198
alisoncasey <96582996+alisoncasey@users.noreply.github.com>
2021-12-28 14:06:06 +0000

    Update contract.py

--- contract.py
+++ contract.py
@@ -8,7 +8,7 @@ from web3.providers.websocket import WebsocketProvider
 from web3 import Web3
 from solc import compile_standard
 
-with open("contract.py") as c:
+with open("contract.sol") as c:
   contractText=c.read()
 with open(".pk") as pkfile:
   privateKey=pkfile.read()
